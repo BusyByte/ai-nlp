0cdb4e1fb7383140142073b1746e4d3c18dd1ebb
Shawn Garner <shawndgarner@gmail.com>
2013-04-29 01:55:43 +0100

    Adding LetterFrequencyTest

---
+++ src/test/scala/net/nomadicalien/nlp/KnownWordsTest.scala
@@ -0,0 +1,11 @@
+package net.nomadicalien.nlp
+
+import org.scalatest.junit.JUnitSuite
+
+/**
+ * User: Shawn Garner
+ * Created: 4/28/13 6:50 PM
+ */
+class KnownWordsTest extends JUnitSuite {
+
+}
