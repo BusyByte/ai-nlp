0a2f39b7c3dd204b943b4b4a00fbeb04d7262cf1
Shawn Garner <shawndgarner@gmail.com>
2014-12-14 14:41:33 +0000

    Removed commons lang

--- src/main/scala/net/nomadicalien/nlp/App.scala
+++ src/main/scala/net/nomadicalien/nlp/App.scala
@@ -1,24 +1,17 @@
 package net.nomadicalien.nlp
 
-import org.apache.commons.lang3.time.StopWatch
-
 /**
  * @author Shawn Garner
  */
 object App extends Logging {
-  val stopwatch = new StopWatch()
-
   def main(args : Array[String]) {
-    stopwatch.start()
+    val startTime = System.currentTimeMillis()
     val stringToDecode = "Esp qtcde nzyqpcpynp zy esp ezatn zq Lcetqtntlw Tyepwwtrpynp hld spwo le Olcexzfes Nzwwprp ty estd jplc."
     val solution = "The first conference on the topic of Artificial Intelligence was held at Dartmouth College in this year."
     val nlp2 = new NLP2(stringToDecode, solution)
     val maxProbSentence = nlp2.process()
     logger.info(s"Result is: $maxProbSentence")
-    stopwatch.stop()
-    logger.info(s"took ${(stopwatch.getTime() / 1000)} seconds")
+    val endTime = System.currentTimeMillis()
+    logger.info(s"took ${((endTime - startTime) / 1000)} seconds")
   }
-
-
-
 }
