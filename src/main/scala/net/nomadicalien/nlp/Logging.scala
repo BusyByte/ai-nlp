4b94911cec92e49a2c3f23f7f8c4266e3153757f
Shawn Garner <shawndgarner@gmail.com>
2014-12-14 15:36:07 +0000

    Converted to log4j 2

--- src/main/scala/net/nomadicalien/nlp/Logging.scala
+++ src/main/scala/net/nomadicalien/nlp/Logging.scala
@@ -1,11 +1,11 @@
 package net.nomadicalien.nlp
 
-import org.apache.log4j.Logger
+import org.apache.logging.log4j.LogManager
 
 /**
  * User: Shawn Garner
  * Created: 4/21/13 9:19 AM
  */
 trait Logging {
-  lazy val logger = Logger.getLogger(this.getClass.getName)
+  lazy val logger = LogManager.getLogger(this.getClass)
 }
