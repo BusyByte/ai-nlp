73f250a77d0ed70197ffbd3805afc36a5c6cd7a8
Shawn Garner <shawndgarner@gmail.com>
2016-02-27 11:15:51 -0600

    set PermutationGenerator up as an ActorPublisher

--- src/main/scala/net/nomadicalien/nlp/actor/PermutationGenerator.scala
+++ src/main/scala/net/nomadicalien/nlp/actor/PermutationGenerator.scala
@@ -1,42 +1,35 @@
 package net.nomadicalien.nlp.actor
 
-import akka.actor._
-import akka.routing._
-import net.nomadicalien.nlp.{Logging, Sentence}
-import scala.collection.mutable
+import akka.stream.actor.ActorPublisher
+import net.nomadicalien.nlp.Logging
 
-/**
- * Created by Shawn on 12/16/2014.
- */
-class PermutationGenerator(encryptedSentence: Sentence) extends Actor with Logging {
-  val workerRegistry = mutable.Set[ActorRef]()
+import scala.annotation.tailrec
+
+class PermutationGenerator extends ActorPublisher[Permutation] with Logging {
+  import akka.stream.actor.ActorPublisherMessage._
+
   val perms = ('a' to 'z').toList.permutations
 
-  override def preStart() = (1 to numWorkers).foreach {number => context.actorOf(Props(classOf[SentenceGenerator], encryptedSentence),s"SentenceGenerator$number")}
   override def receive: Receive = {
-    case Start =>
-
-    case RegisterWorker =>
-      val theSender = sender()
-      if(!workerRegistry.contains(theSender)) {
-        context.watch(theSender)
-      }
-      sendWork(theSender)
-    case SendMoreWork =>
-      sendWork(sender())
-    case Terminated(theActorRef) =>
-      workerRegistry.remove(theActorRef)
-      if(workerRegistry.isEmpty) {
-        context.system.eventStream.publish(CompleteResult())
-        context.stop(self)
-      }
+    case Request(_) =>
+      deliver()
+    case Cancel =>
+      context.stop(self)
   }
 
-  private def sendWork(theActor: ActorRef): Unit = {
-    if(perms.hasNext) {
-      perms.take(batchSize).foreach(theActor ! Permutation(_))
-    } else {
-      workerRegistry.foreach(_ ! Kill)
+
+  @tailrec final def deliver(): Unit =
+    if (totalDemand > 0) {
+      val numToTake: Int = {
+        if (totalDemand <= Int.MaxValue) {
+          totalDemand.toInt
+        } else {
+          Int.MaxValue
+        }
+      }
+      perms.take(numToTake).map(Permutation).foreach(onNext)
+
+      deliver()
     }
-  }
+
 }
