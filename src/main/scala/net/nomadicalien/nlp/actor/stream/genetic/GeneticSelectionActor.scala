eb59993997fad3f34c8035089d40978fd362b88d
Shawn Garner <shawndgarner@gmail.com>
2016-06-18 10:58:58 -0500

    getting rid of the notion of set of selection and keeping just priority queue

--- src/main/scala/net/nomadicalien/nlp/actor/stream/genetic/GeneticSelectionActor.scala
+++ src/main/scala/net/nomadicalien/nlp/actor/stream/genetic/GeneticSelectionActor.scala
@@ -13,8 +13,10 @@ import scala.concurrent.ExecutionContext
 import scala.concurrent.duration.FiniteDuration
 import scala.util.Random
 
-class GeneticSelectionActor(val solutionSentence: Sentence) extends ActorSubscriber with Logging{
+class GeneticSelectionActor(val solutionSentence: Sentence) extends ActorSubscriber with Logging {
+
   import scala.collection.mutable
+
   val rng = new Random()
 
   implicit val ordering: Ordering[Sentence] = new Ordering[Sentence] {
@@ -22,7 +24,6 @@ class GeneticSelectionActor(val solutionSentence: Sentence) extends ActorSubscri
   }
 
   val bestMatchQueue = new mutable.PriorityQueue[Sentence]
-  val currentSet = mutable.Set.empty[Sentence]
 
   override val requestStrategy = new MaxInFlightRequestStrategy(max = 10) {
     override def inFlightInternally: Int = 0
@@ -30,35 +31,25 @@ class GeneticSelectionActor(val solutionSentence: Sentence) extends ActorSubscri
 
   def receive = {
     case OnNext(s: Sentence) =>
-      if(rng.nextInt(100000) == 0) {
+      if (rng.nextInt(100000) == 0) {
         logSentence("SANITY CHECK", s)
       }
 
-      if(!currentSet.contains(s)) {
-        bestMatchQueue.enqueue(s)
-        currentSet.add(s)
-        if(s == solutionSentence) {
-          logSentence("***FOUND IT***", s)
-          logger.info("!!shutting down!!!")
-          implicit val ec: ExecutionContext = context.system.dispatcher
-          context.system.scheduler.scheduleOnce(FiniteDuration(2l, TimeUnit.SECONDS), new Runnable {
-            def run() = {context.system.terminate()}
-          })
+      bestMatchQueue.enqueue(s)
 
-        }
-      }
-
-
-      if(bestMatchQueue.size > 100000) {
+      if (bestMatchQueue.size > 100000) {
         logSentence("BEST", bestMatchQueue.head)
         val best = Stream.continually {
           bestMatchQueue.dequeue()
         }.take(10000).toVector
         bestMatchQueue.clear()
-        //bestMatchQueue.enqueue(best :_*)
-        currentSet.clear()
-        //currentSet ++= best
         context.system.eventStream.publish(ReplacePool(best))
+      }
+
+      if (s == solutionSentence) {
+        logSentence("***FOUND IT***", s)
+        logger.info("!!shutting down!!!")
+        context.system.terminate()
       }
   }
 }
